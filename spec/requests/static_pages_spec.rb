5d03fb22f9f001ebb8a363df7f3f86fc11b8bd2f
Matt ODell <mrmattodell@gmail.com>
2013-07-29 09:29:18 -0400

    Finish layout exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,8 +16,9 @@ describe "StaticPages" do
     before { visit root_path }
     
     let(:heading) { 'Sample App' }
-	
 	  let(:page_title) { '' }
+	  
+	  it_should_behave_like "all static pages"
 	
 	  it { should_not have_selector('title', text: '| Home') }
 
@@ -28,8 +29,9 @@ describe "StaticPages" do
     before { visit help_path }
     
     let(:heading) { 'Help' }
-  
     let(:page_title) { 'Help' }
+    
+    it_should_behave_like "all static pages"
  
   end
   
@@ -38,8 +40,9 @@ describe "StaticPages" do
     before { visit about_path }
     
     let(:heading) { 'About' }
-  
     let(:page_title) { 'About' }
+    
+    it_should_behave_like "all static pages"
  
   end
   
@@ -48,8 +51,29 @@ describe "StaticPages" do
     before { visit contact_path }
     
     let(:heading) { 'Contact' }
-  
     let(:page_title) { 'Contact' }
+    
+    it_should_behave_like "all static pages"
  
   end
+  
+  it "should have the right links to the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
+  end
+    
 end
